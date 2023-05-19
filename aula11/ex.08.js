800336e1e64837176d082d5871531950315232f5
Gui-HR <guilhermehuertar@gmail.com>
2023-05-19 09:37:11 -0300

    Aula11

---
+++ aula11/ex.08.js
@@ -0,0 +1,8 @@
+
+var velocidade = 70
+var velocidadeMaxima = 60
+console.log(`A velocidade do seu carro é de ${velocidade}km/h.`);
+if (velocidade > velocidadeMaxima) {
+    console.log('Você ultrapassou a velocidade permitida de ' + velocidadeMaxima + 'km/h. Multado!')
+}
+console.log('Dirija sempre com segurança.');

2ced911a979682fd574cdfa1e65157dc7485bc81
Gui-HR <guilhermehuertar@gmail.com>
2023-05-20 15:11:25 -0300

    Ex15

--- aula12ex/ex14/script.js
+++ aula12ex/ex14/script.js
@@ -1,17 +1,32 @@
 
+function carregarPagina() {
+    var body = document.querySelector('body')
+    var horaMensagem = document.querySelector('#hora-mensagem')
+    var bomMensagem = document.querySelector('#bom-mensagem')
+    var imagem = document.querySelector('#imagem')
+    var hora = new Date().getHours()
+    var minutos = new Date().getMinutes()
+    if (minutos < 10) {
+        horaMensagem.innerHTML = `Agora são ${hora}:0${minutos}`
+    } else {
+        horaMensagem.innerHTML = `Agora são ${hora}:${minutos}`
+    }
 
-
-function 
-var mensagem = document.querySelector('#mensagem')
-var divFoto = document.querySelector('#foto')
-var imagem = document.querySelector('#imagem')
-var hora = new Date().getHours()
-var minutos = new Date().getMinutes()
-
-if (minutos < 10) {
-    mensagem.innerHTML = `Agora são ${hora}:0${minutos}`
-} else {
-    mensagem.innerHTML = `Agora são ${hora}:${minutos}`
+    if (hora >= 8 && hora <= 12) {
+        bomMensagem.innerText = 'Bom Dia!'
+        imagem.src = 'img/manha.jpg'
+        body.style.backgroundColor = '#ee8a51'
+    } else if (hora > 12 && hora <= 18) {
+        bomMensagem.innerText = 'Boa Tarde!'
+        imagem.src = 'img/tarde.jpg'
+        body.style.backgroundColor = '#ff6145'
+    } else {
+        bomMensagem.innerText = 'Boa Noite!'
+        imagem.src = 'img/noite.jpg'
+        body.style.backgroundColor = '#2c3b42'
+    }
 }
+
+
 
 
--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,15 @@
+var botao = document.querySelector('#botao')
 
+
+botao.addEventListener('click', verificar)
+
+function verificar() {
+    var anoAtual = new Date().getFullYear
+    var anoNascimentoSrting = document.querySelector('#ano')
+
+    if (anoNascimentoSrting.value.length == 0 || anoNascimentoSrting > anoAtual) {
+        alert('[Erro] Verifique os dados e tente novamente')
+    } else {
+
+    }
+}

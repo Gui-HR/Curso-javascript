e9c2a1b049f8fc1f20800f12d43ccdabe7ad7433
Gui-HR <guilhermehuertar@gmail.com>
2023-05-21 15:21:28 -0300

    Exercicio 15 ainda
    Fiz a parte de validação apenas

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,15 +1,23 @@
-var botao = document.querySelector('#botao')
-
-
-botao.addEventListener('click', verificar)
-
 function verificar() {
-    var anoAtual = new Date().getFullYear
-    var anoNascimentoSrting = document.querySelector('#ano')
+    var anoAtual = new Date().getFullYear()
+    var anoNascimento = document.querySelector('#string-ano')
+    var resultado = document.querySelector('#resultado')
 
-    if (anoNascimentoSrting.value.length == 0 || anoNascimentoSrting > anoAtual) {
-        alert('[Erro] Verifique os dados e tente novamente')
+    if (anoNascimento.value == 0 || anoNascimento.value > anoAtual) {
+        alert('[ERRO] Verifique os dados e tente novamente')
     } else {
+        var masculino = document.getElementsByName('sexo')[0]
+        var feminino = document.getElementsByName('sexo')[1]
+        var idade = anoAtual - Number(anoNascimento.value)
+        var genero
+        
+        if (masculino.checked) {
+            genero = 'homem'
+        } else if (feminino.checked) {
+            genero = 'mulher'
+        }
+
+        console.log(genero);
 
     }
 }

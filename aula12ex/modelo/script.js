f8eaf6527de5b62bcc46a86ee62243455c0cdf3d
Gui-HR <guilhermehuertar@gmail.com>
2023-05-22 11:34:27 -0300

    Ex15 finalizado

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -9,15 +9,49 @@ function verificar() {
         var masculino = document.getElementsByName('sexo')[0]
         var feminino = document.getElementsByName('sexo')[1]
         var idade = anoAtual - Number(anoNascimento.value)
-        var genero
+        var genero = 'nenhum'
         
         if (masculino.checked) {
             genero = 'homem'
         } else if (feminino.checked) {
             genero = 'mulher'
+        } else {
+            alert('[ERRO] Verifique os dados e tente novamente')
         }
-
         console.log(genero);
+
+        var pResultado = document.querySelector('#p-resultado')
+        var imagem = document.querySelector('#imagem')
+
+        if (genero == 'homem' && genero != 'nenhum') {
+            if (idade <= 10) {
+                pResultado.innerText = 'Homem de ' + idade + ' anos'
+                imagem.src = 'img/crianca-homem.jpg'
+            } else if (idade <= 25 ) {
+                pResultado.innerText = 'Homem de ' + idade + ' anos'
+                imagem.src = 'img/jovem-homem.jpg'
+            } else if (idade < 50) {
+                pResultado.innerText = 'Homem de ' + idade + ' anos'
+                imagem.src = 'img/adulto-homem.jpg'
+            } else if (idade >= 50) {
+                pResultado.innerText = 'Homem de ' + idade + ' anos'
+                imagem.src = 'img/idoso-homem.jpg'
+            }
+        } else if (genero == 'mulher' && genero != 'nenhum'){
+            if (idade <= 10) {
+                pResultado.innerText = 'Mulher de ' + idade + ' anos'
+                imagem.src = 'img/crianca-mulher.jpg'
+            } else if (idade <= 25 ) {
+                pResultado.innerText = 'Mulher de ' + idade + ' anos'
+                imagem.src = 'img/jovem-mulher.jpg'
+            } else if (idade < 50) {
+                pResultado.innerText = 'Mulher de ' + idade + ' anos'
+                imagem.src = 'img/adulto-mulher.jpg'
+            } else if (idade >= 50) {
+                pResultado.innerText = 'Mulher de ' + idade + ' anos'
+                imagem.src = 'img/idoso-mulher.jpg'
+            }
+        }
 
     }
 }
